f88f3ed20a4ad3e5c8be8bf98e530bc9592b1ba7
Alex the wizard <sashok1111122222@gmail.com>
2024-06-19 21:21:21 +0300

    feat: align test with new conventions

--- tests/test_ledger_attachments.py
+++ tests/test_ledger_attachments.py
@@ -2,10 +2,45 @@
 Unit tests for listing, attaching and detaching remote files.
 """
 
-from typing import List
 import pytest
 import pandas as pd
+from typing import List
+from io import StringIO
 from cashctrl_ledger import CashCtrlLedger
+
+LEDGER_CSV = """
+    id,   date, account, counter_account,  currency, amount,      vat_code, text,                             document
+    1, 2024-05-24, 2100,            2200,       CHF,    100, Test_VAT_code, pytest single transaction 1,
+    2, 2024-05-24, 2100,            2200,       CHF,    100, Test_VAT_code, pytest single transaction 1,      /file1.txt
+    3, 2024-05-24, 2100,            2200,       CHF,    100, Test_VAT_code, pytest single transaction 1,      /subdir/file2.txt
+    4, 2024-05-24, 2100,            2200,       CHF,    100, Test_VAT_code, pytest single transaction 1,      /file1.txt
+"""
+
+VAT_CSV = """
+    id,             rate, account, inclusive, text
+    Test_VAT_code,  0.02,   22000,      True, Input Tax 2%
+"""
+
+TEST_VAT_CODE = pd.read_csv(StringIO(VAT_CSV), skipinitialspace=True)
+LEDGER_ENTRIES = pd.read_csv(StringIO(LEDGER_CSV), skipinitialspace=True)
+
+@pytest.fixture(scope="session")
+def ledger_attached_ids():
+    cashctrl = CashCtrlLedger()
+
+    # Fetch original state
+    initial_vat_codes = cashctrl.vat_codes().reset_index()
+
+    # Create test VAT code and ledger
+    cashctrl.mirror_vat_codes(TEST_VAT_CODE, delete=False)
+    ledger_ids = [cashctrl.add_ledger_entry(LEDGER_ENTRIES.query(f'id == {i}')) for i in range(1, 5)]
+
+    yield ledger_ids
+
+    # Restore initial state
+
+    cashctrl.delete_ledger_entry([str(id) for id in ledger_ids])
+    cashctrl.mirror_vat_codes(initial_vat_codes, delete=True)
 
 @pytest.fixture(scope="module")
 def tmp_path_for_module(tmp_path_factory):
@@ -79,27 +114,23 @@ def test_get_ledger_attachments(files, ledger_ids):
     expected = initial | {ledger_ids[0]: ['/file1.txt'], ledger_ids[1]:  ['/file1.txt', '/subdir/file2.txt']}
     assert sort_dict_values(engine._get_ledger_attachments()) == sort_dict_values(expected)
 
-def test_attach_ledger_files(files, ledger_with_attachments_ids):
-
-
-    # taaaable
-
+def test_attach_ledger_files(files, ledger_attached_ids):
     engine = CashCtrlLedger()
-    # Attach file that should be updated
-    engine._client.post("journal/update_attachments.json", data={'id': ledger_with_attachments_ids[1], 'fileIds': files['id'].iat[0]})
-    # Attach file that should left untouched
-    engine._client.post("journal/update_attachments.json", data={'id': ledger_with_attachments_ids[2], 'fileIds': files['id'].iat[0]})
     # Attach file that should be deleted
-    engine._client.post("journal/update_attachments.json", data={'id': ledger_with_attachments_ids[0], 'fileIds': files['id'].iat[0]})
+    engine._client.post("journal/update_attachments.json", data={'id': ledger_attached_ids[0], 'fileIds': files['id'].iat[0]})
+    # Attach file that should be updated
+    engine._client.post("journal/update_attachments.json", data={'id': ledger_attached_ids[1], 'fileIds': files['id'].iat[0]})
+    # Attach file that should left untouched
+    engine._client.post("journal/update_attachments.json", data={'id': ledger_attached_ids[2], 'fileIds': files['id'].iat[0]})
 
     # Update attachments with detach=False
     engine.attach_ledger_files(detach=False)
     attachments = engine._get_ledger_attachments()
     expected = {
-        ledger_with_attachments_ids[0]: ['/file1.txt'],
-        ledger_with_attachments_ids[1]: ['/subdir/file2.txt'],
-        ledger_with_attachments_ids[2]: ['/file1.txt'],
-        ledger_with_attachments_ids[3]: ['/file1.txt'],
+        ledger_attached_ids[0]: ['/file1.txt'],
+        ledger_attached_ids[1]: ['/subdir/file2.txt'],
+        ledger_attached_ids[2]: ['/file1.txt'],
+        ledger_attached_ids[3]: ['/file1.txt'],
     }
     assert sort_dict_values(expected) == sort_dict_values(attachments)
 
@@ -107,8 +138,8 @@ def test_attach_ledger_files(files, ledger_with_attachments_ids):
     engine.attach_ledger_files(detach=True)
     attachments = engine._get_ledger_attachments()
     expected = {
-        ledger_with_attachments_ids[1]: ['/subdir/file2.txt'],
-        ledger_with_attachments_ids[2]: ['/file1.txt'],
-        ledger_with_attachments_ids[3]: ['/file1.txt'],
+        ledger_attached_ids[1]: ['/subdir/file2.txt'],
+        ledger_attached_ids[2]: ['/file1.txt'],
+        ledger_attached_ids[3]: ['/file1.txt'],
     }
     assert sort_dict_values(expected) == sort_dict_values(attachments)
