1fb62c4cc398e1cc8658d0bb86ff2a776af27e04
Alex the wizard <sashok1111122222@gmail.com>
2024-07-18 10:29:54 +0300

    feat: use consistent_df package

--- tests/test_ledger.py
+++ tests/test_ledger.py
@@ -2,7 +2,8 @@
 
 from io import StringIO
 from typing import List
-from cashctrl_ledger import assert_frame_equal, CashCtrlLedger, df_to_consistent_str, nest
+from cashctrl_ledger import CashCtrlLedger
+from consistent_df import assert_frame_equal, df_to_consistent_str, nest
 import pandas as pd
 import pytest
 from requests.exceptions import RequestException
