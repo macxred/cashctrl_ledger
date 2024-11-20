144cf7530c672585a1c1e1aba3f23bbc8e562e9c
Alex the wizard <sashok1111122222@gmail.com>
2024-11-20 22:52:20 +0200

    drop: useless tests from accounts test suit for now

--- cashctrl_ledger/tests/test_accounts.py
+++ cashctrl_ledger/tests/test_accounts.py
@@ -3,7 +3,7 @@
 from io import StringIO
 import pandas as pd
 import pytest
-from pyledger.tests.base_accounts import BaseTestAccounts
+from pyledger.tests import BaseTestAccounts
 # flake8: noqa: F401
 from base_test import initial_ledger
 from requests.exceptions import RequestException
@@ -14,19 +14,6 @@ ACCOUNT_CSV = """
     /Balance,         9990,      EUR,         , Test EUR Bank Account
     /Balance/Node,    9993,      EUR,         , Transitory Account EUR
 """
-
-LEDGER_CSV = """
-    id,     date, account, contra, currency,     amount, report_amount, description
-    1,  2024-01-21,  9992,   9995,      CHF,     100.00,              , transaction 1
-    2,  2024-02-22,  9991,   9994,      USD,     100.00,         88.88, transaction 2
-    3,  2024-03-23,  9991,       ,      USD,     100.00,         85.55, transaction 3
-    3,  2024-03-23,      ,   9995,      CHF,      85.55,              , transaction 3
-    4,  2024-04-24,  9994,   9991,      USD,     100.00,         77.77, transaction 4
-    5,  2024-05-25,  9992,   9995,      CHF,      10.00,              , transaction 5
-    6,  2024-06-26,  9995,       ,      CHF,      95.55,              , transaction 6
-    6,  2024-06-26,      ,   9991,      USD,     100.00,         95.55, transaction 6
-"""
-LEDGER_ENTRIES = pd.read_csv(StringIO(LEDGER_CSV), skipinitialspace=True)
 ACCOUNTS = pd.read_csv(StringIO(ACCOUNT_CSV), skipinitialspace=True)
 
 
@@ -35,41 +22,6 @@ class TestAccounts(BaseTestAccounts):
     def ledger(self, initial_ledger):
         initial_ledger.clear()
         return initial_ledger
-
-    @pytest.fixture(scope="class")
-    def ledger_with_balance(self, initial_ledger):
-        initial_ledger.restore(accounts=self.ACCOUNTS, ledger=LEDGER_ENTRIES, settings=self.SETTINGS)
-        return initial_ledger
-
-    # TODO: Move this test to the PyLedger package when storing Price history implemented
-    @pytest.mark.parametrize(
-        "account, date, expected",
-        [
-            (9991, "2024-02-21", {"USD": 0.00, "reporting_currency": 0.00}),
-            (9991, "2024-02-22", {"USD": 100.00, "reporting_currency": 88.88}),
-            (9991, "2024-03-23", {"USD": 200.00, "reporting_currency": 174.43}),
-            (9991, "2024-04-24", {"USD": 100.00, "reporting_currency": 96.66}),
-            (9991, "2024-06-26", {"USD": 0.00, "reporting_currency": 1.11}),
-            (9991, None, {"USD": 0.00, "reporting_currency": 1.11}),
-            (9992, "2024-01-20", {"CHF": 0.00, "reporting_currency": 0.00}),
-            (9992, "2024-01-21", {"CHF": 100.00, "reporting_currency": 100.00}),
-            (9992, "2024-05-25", {"CHF": 110.00, "reporting_currency": 110.00}),
-            (9992, None, {"CHF": 110.00, "reporting_currency": 110.00}),
-            (9994, "2024-02-21", {"USD": 0.00, "reporting_currency": 0.00}),
-            (9994, "2024-02-22", {"USD": -100.00, "reporting_currency": -88.88}),
-            (9994, "2024-04-24", {"USD": 0.00, "reporting_currency": -11.11}),
-            (9994, None, {"USD": 0.00, "reporting_currency": -11.11}),
-            (9995, "2024-01-20", {"CHF": 0.00, "reporting_currency": 0.00}),
-            (9995, "2024-01-21", {"CHF": -100.00, "reporting_currency": -100.00}),
-            (9995, "2024-03-23", {"CHF": -185.55, "reporting_currency": -185.55}),
-            (9995, "2024-05-25", {"CHF": -195.55, "reporting_currency": -195.55}),
-            (9995, "2024-06-26", {"CHF": -100.00, "reporting_currency": -100.00}),
-            (9995, None, {"CHF": -100.00, "reporting_currency": -100.00}),
-        ],
-    )
-    def test_account_single_balance(self, ledger_with_balance, account, date, expected):
-        balance = ledger_with_balance._single_account_balance(account=account, date=date)
-        assert balance == expected
 
     def test_add_already_existed_raise_error(self, ledger):
         super().test_add_already_existed_raise_error(
