1e41282e8b843f2979e00dda3054beb05396b994
Alex the wizard <sashok1111122222@gmail.com>
2024-11-21 11:18:54 +0200

    feat: add CashCtrlAccountingEntity

---
+++ cashctrl_ledger/cashctrl_accounuting_entity.py
@@ -0,0 +1,22 @@
+"""Abstract base class for storing tabular entities in CashCtrl."""
+
+from pyledger import AccountingEntity
+from cashctrl_api import CashCtrlClient
+
+
+class CashCtrlAccountingEntity(AccountingEntity):
+    """Abstract base class for storing tabular entities in CashCtrl."""
+
+    _client: CashCtrlClient = None
+
+    def __init__(self, client: CashCtrlClient, *args, **kwargs):
+        """
+        Initialize the CashCtrlAccountingEntity.
+
+        Args:
+            client (CashCtrlClient): The client instance used to interact with the CashCtrl API.
+            *args: Additional positional arguments passed to the superclass.
+            **kwargs: Additional keyword arguments passed to the superclass.
+        """
+        self._client = client
+        super().__init__(*args, **kwargs)
