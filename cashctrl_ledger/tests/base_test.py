343275bf7c2b8c28774b0e4ae30ffdab8afa2747
Alex the wizard <sashok1111122222@gmail.com>
2024-11-28 21:00:38 +0200

    reword: ledger to engine in base test

--- cashctrl_ledger/tests/base_test.py
+++ cashctrl_ledger/tests/base_test.py
@@ -5,17 +5,16 @@ from cashctrl_ledger import ExtendedCashCtrlLedger
 @pytest.fixture(scope="module")
 def initial_engine(tmp_path_factory):
     tmp_path = tmp_path_factory.mktemp("temp")
-    ledger = ExtendedCashCtrlLedger(
+    engine = ExtendedCashCtrlLedger(
         transitory_account=9999,
         price_history_path=tmp_path / "price_history.csv"
     )
-
-    ledger.dump_to_zip(tmp_path / "ledger.zip")
+    engine.dump_to_zip(tmp_path / "ledger.zip")
     # Hack to create JPY currency
-    currency_id = ledger._client.post("currency/create.json", {"code": "JPY"})["insertId"]
+    currency_id = engine._client.post("currency/create.json", {"code": "JPY"})["insertId"]
 
-    yield ledger
+    yield engine
 
-    ledger.restore_from_zip(tmp_path / "ledger.zip")
-    ledger.accounts.delete([{"account": 9999}])
-    ledger._client.post("currency/delete.json", {"ids": currency_id})
+    engine.restore_from_zip(tmp_path / "ledger.zip")
+    engine.accounts.delete([{"account": 9999}])
+    engine._client.post("currency/delete.json", {"ids": currency_id})
