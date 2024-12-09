71ecda8e73bcfb6957dbed033718c057725899e2
Alex the wizard <sashok1111122222@gmail.com>
2024-12-09 13:57:37 +0200

    refactor: update test suit for fiscal periods

--- cashctrl_ledger/tests/test_fiscal_periods.py
+++ cashctrl_ledger/tests/test_fiscal_periods.py
@@ -1,8 +1,7 @@
 """Unit tests for fiscal periods operations."""
 
-from io import StringIO
-from cashctrl_ledger import CashCtrlLedger
 import pandas as pd
+from cashctrl_ledger import CashCtrlLedger
 import pytest
 
 
@@ -21,10 +20,43 @@ def engine():
     created_ids = set(new_ids) - set(initial_ids)
     if len(created_ids):
         ids = ",".join([str(id) for id in created_ids])
-        breakpoint()
         cashctrl._client.post("fiscalperiod/delete.json", params={"ids": ids})
 
 def test_fiscal_period_exist(engine):
-    fp = engine.fiscal_period_list()
+    fiscal_periods = engine.fiscal_period_list()
+    earliest_start = fiscal_periods["start"].min()
+    latest_end = fiscal_periods["end"].max()
 
-    engine.ensure_fiscal_periods_exist('2022-01-01', '2028-12-31')
+    # Extend 3 years in the past and future
+    past_extension = earliest_start - pd.DateOffset(years=3)
+    future_extension = latest_end + pd.DateOffset(years=3)
+    engine.ensure_fiscal_periods_exist(start=past_extension.date(), end=future_extension.date())
+    updated_fiscal_periods = engine.fiscal_period_list()
+
+    assert len(updated_fiscal_periods) - len(fiscal_periods) == 6, (
+        "Should create 6 new fiscal periods"
+    )
+
+    # Initialize the first expected start and end dates
+    first_start = past_extension
+    first_end = (pd.Timestamp(past_extension.year, 12, 31))
+
+    # Loop over all fiscal periods and validate start and end dates
+    for i, row in updated_fiscal_periods.iterrows():
+        expected_start = first_start + pd.DateOffset(years=i)
+        expected_end = first_end + pd.DateOffset(years=i)
+        assert row["start"] == expected_start, (
+            f"Fiscal period start date {row["start"]} does not match expected {expected_start}"
+        )
+        assert row["end"] == expected_end, (
+            f"Fiscal period end date {row["end"]} does not match expected {expected_end}"
+        )
+
+def test_fiscal_period_list_raise_error_with_gap(engine):
+    fiscal_periods = engine.fiscal_period_list()
+    latest_end = fiscal_periods["end"].max()
+    new_start = latest_end + pd.DateOffset(years=1)
+    new_end = latest_end + pd.DateOffset(years=2)
+    engine.fiscal_period_add(start=new_start, end=new_end, name="test_fiscal_period")
+    with pytest.raises(ValueError, match="Gaps between fiscal periods."):
+        engine.fiscal_period_list()
