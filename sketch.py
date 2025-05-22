3fa14c42f9707f7fcde219c8075093b86b9e2e27
Alex the wizard <sashok1111122222@gmail.com>
2025-05-22 14:04:58 +0300

    feat: streamline sketch

--- sketch.py
+++ sketch.py
@@ -31,47 +31,22 @@ id,       date, account, contra, currency,    amount, report_amount, tax_code, d
 """
 JOURNAL = pd.read_csv(StringIO(JOURNAL_CSV), skipinitialspace=True)
 
-memoryLedger = MemoryLedger()
-memoryLedger.restore(configuration={"REPORTING_CURRENCY": "USD"}, accounts=ACCOUNTS, price_history=PRICES, journal=JOURNAL)
-cashctrl = ExtendedCashCtrlLedger(transitory_account=1999)
-cashctrl.restore(configuration={"REPORTING_CURRENCY": "USD"}, accounts=ACCOUNTS, price_history=PRICES, journal=JOURNAL)
-
-def extract_nodes(data, parent_path="", level=0):
-    """
-    Recursively extracts node information from a nested structure of financial categories and accounts.
-
-    Args:
-        data (list): The list of dictionaries representing the hierarchy.
-        parent_path (list): The trail of parent node 'text' attributes.
-        level (int): Current depth level for clarity (optional).
-
-    Returns:
-        list of dicts: Flattened list of extracted node data.
-    """
-    result = []
-    for item in data:
-        path = f"{parent_path}/{item.get('text', '')}"
-        if item.get('leaf', False):
-            result.append(item | {"path": path})
-        elif 'data' in item and isinstance(item['data'], list):
-            result.extend(extract_nodes(item['data'], parent_path=path, level=level+1))
-        else:
-            raise ValueError(f"Unexpected data at {path}.")
-    return result
-
-source = memoryLedger
+source = MemoryLedger()
+source.restore(configuration={"REPORTING_CURRENCY": "USD"}, accounts=ACCOUNTS, price_history=PRICES, journal=JOURNAL)
 remote = ExtendedCashCtrlLedger(transitory_account=1999)
-out = remote.assets.mirror(source.assets.list(), delete=True)
-
-
+remote.restore(configuration={"REPORTING_CURRENCY": "USD"}, accounts=ACCOUNTS, price_history=PRICES, journal=JOURNAL)
 
 # Define the test date ranges
 fiscal_2024_start = date(2024, 1, 12)
 fiscal_2025_end = date(2025, 12, 31)
 account = 4001
 
-print(source.account_balance(account, "2025-12-31"))
-print(remote.account_balance(account, "2025-12-31"))
-response = remote._client.json_request("GET", "report/element/data.json", params={"elementId": 2, "startDate": fiscal_2024_start, "endDate": fiscal_2025_end})
-pnl = pd.DataFrame(extract_nodes(response["data"]))
-print(pnl[["endAmount", "text"]])
+source_balance = source.account_balance(account, "2025-12-31")
+remote_balance = remote.account_balance(account, "2025-12-31")
+
+print(source_balance)
+print(remote_balance)
+# {'reporting_currency': 332.04, 'EUR': 300.0}
+# {'reporting_currency': 332.04, 'EUR': 300.0}
+
+assert source_balance == remote_balance
