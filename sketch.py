05ef3ceb075fe1b01758eb210ea3fdbe36eadfbf
Alex the wizard <sashok1111122222@gmail.com>
2025-12-09 13:14:19 +0200

    debug: add sketch and breakpoint to the potential place with a problem

---
+++ sketch.py
@@ -0,0 +1,67 @@
+"""
+Execute:
+    python -i sketch.py
+"""
+
+from io import StringIO
+import numpy as np
+import pandas as pd
+from pyledger import TextLedger
+from cashctrl_ledger import ExtendedCashCtrlLedger
+
+source = TextLedger()
+engine = ExtendedCashCtrlLedger(1999)
+
+ACCOUNT_CSV = """
+group,    account, currency, description
+/Assets,     1176,      CHF, Accounts Receivable VAT Cleared
+/Assets,     1904,      CAD, Custody account - EUR
+/Assets,     1999,      CHF, Transitory Account for CashCtrl rounding precision
+/Revenue,    6954,      CAD, Interest Income CAD
+"""
+ACCOUNTS = pd.read_csv(StringIO(ACCOUNT_CSV), skipinitialspace=True)
+
+ASSETS_CSV = """
+ticker, increment
+   CAD,      0.01
+   CHF,      0.01
+"""
+ASSETS = pd.read_csv(StringIO(ASSETS_CSV), skipinitialspace=True)
+
+PRICE_CSV = """
+ticker,       date, currency,    price
+   CAD, 2024-01-01,      CHF,   0.6523
+   CAD, 2024-02-01,      CHF,   0.6458
+   CAD, 2024-03-01,      CHF,   0.6545
+   CAD, 2024-04-01,      CHF,   0.6594
+   CAD, 2024-05-01,      CHF,   0.6714
+   CAD, 2024-06-01,      CHF,   0.6725
+   CAD, 2024-07-01,      CHF,   0.6617
+   CAD, 2024-08-01,      CHF,   0.6617
+   CAD, 2024-09-01,      CHF,   0.6369
+   CAD, 2024-10-01,      CHF,   0.6326
+   CAD, 2024-11-01,      CHF,   0.6335
+   CAD, 2024-12-01,      CHF,   0.6346
+"""
+PRICE = pd.read_csv(StringIO(PRICE_CSV), skipinitialspace=True)
+
+
+JOURNAL_CSV = """
+      date, account, contra, currency,     amount, report_amount, description
+2024-09-06,        ,   6954,      CAD,    1618.17,       1015.30, Bruttozins Festgeldanlage 1252851-3G-8
+          ,    1176,       ,      CHF,     355.35,              , Verrechnungssteuer 35% von CHF 1015.30
+          ,    1904,       ,      CAD,    1051.81,        659.95, Nettozins Festgeldanlage 1252851-3G-8
+"""
+JOURNAL = pd.read_csv(StringIO(JOURNAL_CSV), skipinitialspace=True)
+
+
+engine.restore(
+    configuration={"reporting_currency": "CHF"},
+    accounts=ACCOUNTS,
+    assets=ASSETS,
+    price_history=PRICE,
+)
+
+engine.journal.mirror(JOURNAL)
+
+breakpoint()
