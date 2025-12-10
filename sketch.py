1193b7e211a484c13a3b887ea8edefa4e779f22b
Alex the wizard <sashok1111122222@gmail.com>
2025-12-10 19:36:37 +0200

    update sketch to have all invalid transactions

--- sketch.py
+++ sketch.py
@@ -7,12 +7,14 @@ from io import StringIO
 import pandas as pd
 from pyledger import TextLedger
 from cashctrl_ledger import ExtendedCashCtrlLedger
+from consistent_df import assert_frame_equal
 
 source = TextLedger()
 engine = ExtendedCashCtrlLedger(1999)
 
 ACCOUNT_CSV = """
 group,    account, currency, description
+/Assets,     1000,      USD, Cash in Bank USD
 /Assets,     1010,      EUR, Cash in Bank EUR
 /Assets,     1020,      JPY, Cash in Bank JPY
 /Assets,     1999,      USD, Transitory Account for CashCtrl rounding precision
@@ -47,6 +49,8 @@ JOURNAL_CSV = """
       date, account, contra, currency,      amount, report_amount, description
 2024-07-04,    1020,       ,      JPY, 12345678.00,      76386.36, Convert JPY to EUR
           ,    1010,       ,      EUR,   -70791.78,     -76386.36, Convert JPY to EUR
+2024-12-04,        ,   1000,      USD,  9500000.00,              , Convert 9.5 Mio USD at EUR @1.050409356 (9 decimal places)
+          ,    1010,       ,      EUR,  9978888.88,    9500000.00, Convert 9.5 Mio USD at EUR @1.050409356 (9 decimal places)
 """
 JOURNAL = pd.read_csv(StringIO(JOURNAL_CSV), skipinitialspace=True)
 
@@ -57,8 +61,13 @@ engine.restore(
     assets=ASSETS,
     price_history=PRICE,
 )
-engine.journal.mirror(JOURNAL)
-
+initial = engine.sanitize_journal(JOURNAL)
+engine.journal.mirror(initial)
 balance = engine.individual_account_balances(accounts=1999, period="2024")
+
+# Rounding precision compensation
 assert balance.query("account == 1999")["report_balance"].iloc[0] == 0.0
 assert balance.query("account == 1999")["balance"].iloc[0]["USD"] == 0.0
+
+# Currencies mismatch
+assert_frame_equal(engine.journal.list(), initial, check_like=True, ignore_row_order=True, ignore_columns=["id"])
