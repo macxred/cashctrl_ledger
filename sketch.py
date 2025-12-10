e6066646df51a4b4eccedd6bb958bb53f0dcb227
Alex the wizard <sashok1111122222@gmail.com>
2025-12-10 16:38:06 +0200

    sketch: update error example

--- sketch.py
+++ sketch.py
@@ -4,7 +4,6 @@ Execute:
 """
 
 from io import StringIO
-import numpy as np
 import pandas as pd
 from pyledger import TextLedger
 from cashctrl_ledger import ExtendedCashCtrlLedger
@@ -14,74 +13,52 @@ engine = ExtendedCashCtrlLedger(1999)
 
 ACCOUNT_CSV = """
 group,    account, currency, description
-/Assets,     1176,      CHF, Accounts Receivable VAT Cleared
-/Assets,     1903,      USD, Transitory account - USD
-/Assets,     1904,      CAD, Transitory account - CAD
-/Assets,     1999,      CHF, Transitory Account for CashCtrl rounding precision
-/Revenue,    6953,      USD, Interest Income CAUSDD
-/Revenue,    6954,      CAD, Interest Income CAD
+/Assets,     1010,      EUR, Cash in Bank EUR
+/Assets,     1020,      JPY, Cash in Bank JPY
+/Assets,     1999,      USD, Transitory Account for CashCtrl rounding precision
 """
 ACCOUNTS = pd.read_csv(StringIO(ACCOUNT_CSV), skipinitialspace=True)
 
 ASSETS_CSV = """
 ticker, increment
-   CAD,      0.01
    USD,      0.01
-   CHF,      0.01
+   EUR,      0.01
+   JPY,       1.0
 """
 ASSETS = pd.read_csv(StringIO(ASSETS_CSV), skipinitialspace=True)
 
 PRICE_CSV = """
-ticker,       date, currency,    price
-   CAD, 2024-01-01,      CHF,   0.6523
-   CAD, 2024-02-01,      CHF,   0.6458
-   CAD, 2024-03-01,      CHF,   0.6545
-   CAD, 2024-04-01,      CHF,   0.6594
-   CAD, 2024-05-01,      CHF,   0.6714
-   CAD, 2024-06-01,      CHF,   0.6725
-   CAD, 2024-07-01,      CHF,   0.6617
-   CAD, 2024-08-01,      CHF,   0.6617
-   CAD, 2024-09-01,      CHF,   0.6369
-   CAD, 2024-10-01,      CHF,   0.6326
-   CAD, 2024-11-01,      CHF,   0.6335
-   CAD, 2024-12-01,      CHF,   0.6346
-   USD, 2024-01-01,      CHF,   0.8808
-   USD, 2024-02-01,      CHF,   0.8639
-   USD, 2024-03-01,      CHF,   0.8817
-   USD, 2024-04-01,      CHF,   0.8918
-   USD, 2024-05-01,      CHF,   0.9165
-   USD, 2024-06-01,      CHF,   0.9197
-   USD, 2024-07-01,      CHF,   0.9061
-   USD, 2024-08-01,      CHF,   0.9049
-   USD, 2024-09-01,      CHF,   0.8754
-   USD, 2024-10-01,      CHF,   0.8564
-   USD, 2024-11-01,      CHF,    0.865
-   USD, 2024-12-01,      CHF,   0.8846
+ticker,       date, currency,  price
+   CHF, 2023-12-29,      USD,  0.007
+   EUR, 2023-12-29,      USD, 1.1068
+   EUR, 2024-03-29,      USD, 1.0794
+   EUR, 2024-06-28,      USD, 1.0708
+   EUR, 2024-09-30,      USD,  1.117
+   JPY, 2023-12-29,      USD, 0.0071
+   JPY, 2024-03-29,      USD, 0.0066
+   JPY, 2024-06-28,      USD, 0.0062
+   JPY, 2024-09-30,      USD,  0.007
+
 """
 PRICE = pd.read_csv(StringIO(PRICE_CSV), skipinitialspace=True)
 
 
 JOURNAL_CSV = """
-      date, account, contra, currency,     amount, report_amount, description
-2024-08-26,        ,   6953,      USD,    2408.10,       2060.85, Bruttozins Festgeldanlage 1252851-3G-6
-          ,    1176,       ,      CHF,     721.30,              , Verrechnungssteuer 35% von CHF 2060.85
-          ,    1903,       ,      USD,    1565.27,       1339.55, Nettozins Festgeldanlage 1252851-3G-6
-2024-09-06,        ,   6954,      CAD,    1618.17,       1015.30, Bruttozins Festgeldanlage 1252851-3G-8
-          ,    1176,       ,      CHF,     355.35,              , Verrechnungssteuer 35% von CHF 1015.30
-          ,    1904,       ,      CAD,    1051.81,        659.95, Nettozins Festgeldanlage 1252851-3G-8
-2024-10-11,        ,   6954,      CAD,    1522.11,        955.50, Bruttozins Festgeldanlage 1252851-3G-8
-          ,    1176,       ,      CHF,     334.40,              , Verrechnungssteuer 35% von CHF 955.50
-          ,    1904,       ,      CAD,     989.37,        621.10, Nettozins Festgeldanlage 1252851-3G-8
+      date, account, contra, currency,      amount, report_amount, description
+2024-07-04,    1020,       ,      JPY, 12345678.00,      76386.36, Convert JPY to EUR
+          ,    1010,       ,      EUR,   -70791.78,     -76386.36, Convert JPY to EUR
 """
 JOURNAL = pd.read_csv(StringIO(JOURNAL_CSV), skipinitialspace=True)
 
 
-engine.reporting_currency = "CHF"
+engine.reporting_currency = "USD"
 engine.restore(
     accounts=ACCOUNTS,
     assets=ASSETS,
     price_history=PRICE,
 )
-
 engine.journal.mirror(JOURNAL)
 
+balance = engine.individual_account_balances(accounts=1999, period="2024")
+assert balance.query("account == 1999")["report_balance"].iloc[0] == 0.0
+assert balance.query("account == 1999")["balance"].iloc[0]["USD"] == 0.0
